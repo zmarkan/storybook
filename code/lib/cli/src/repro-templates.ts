013f9cf99827ee927e096be3af1d6e80a719c40b
Kyle Gach <kyle.gach@gmail.com>
2022-07-27 11:31:45 -0600

    Add `nextjs` and `nextjs_typescript` to repro configs

--- code/lib/cli/src/repro-templates.ts
+++ code/lib/cli/src/repro-templates.ts
@@ -21,6 +21,29 @@ const craTemplates = {
   },
 };
 
+const nextjsTemplates = {
+  'nextjs/default-js': {
+    name: 'Next.js (JavaScript)',
+    script: 'npx create-next-app .',
+    cadence: ['ci', 'daily', 'weekly'],
+    expected: {
+      framework: '@storybook/nextjs',
+      renderer: '@storybook/react',
+      builder: '@storybook/builder-webpack5',
+    },
+  },
+  'nextjs/default-ts': {
+    name: 'Next.js (TypeScript)',
+    script: 'npx create-next-app . --typescript',
+    cadence: ['ci', 'daily', 'weekly'],
+    expected: {
+      framework: '@storybook/nextjs',
+      renderer: '@storybook/react',
+      builder: '@storybook/builder-webpack5',
+    },
+  },
+};
+
 const reactViteTemplates = {
   'react-vite/default-js': {
     name: 'React Vite (JS)',
@@ -39,29 +62,6 @@ const reactViteTemplates = {
     expected: {
       framework: '@storybook/react-vite',
       renderer: '@storybook/react',
-      builder: '@storybook/builder-vite',
-    },
-  },
-};
-
-const vue3ViteTemplates = {
-  'vue3-vite/default-js': {
-    name: 'Vue3 Vite (JS)',
-    script: 'yarn create vite . --template vue',
-    cadence: ['ci', 'daily', 'weekly'],
-    expected: {
-      framework: '@storybook/vue3-vite',
-      renderer: '@storybook/vue3',
-      builder: '@storybook/builder-vite',
-    },
-  },
-  'vue3-vite/default-ts': {
-    name: 'Vue3 Vite (TS)',
-    script: 'yarn create vite . --template vue-ts',
-    cadence: ['ci', 'daily', 'weekly'],
-    expected: {
-      framework: '@storybook/vue3-vite',
-      renderer: '@storybook/vue3',
       builder: '@storybook/builder-vite',
     },
   },
@@ -90,11 +90,35 @@ const svelteViteTemplates = {
   },
 };
 
+const vue3ViteTemplates = {
+  'vue3-vite/default-js': {
+    name: 'Vue3 Vite (JS)',
+    script: 'yarn create vite . --template vue',
+    cadence: ['ci', 'daily', 'weekly'],
+    expected: {
+      framework: '@storybook/vue3-vite',
+      renderer: '@storybook/vue3',
+      builder: '@storybook/builder-vite',
+    },
+  },
+  'vue3-vite/default-ts': {
+    name: 'Vue3 Vite (TS)',
+    script: 'yarn create vite . --template vue-ts',
+    cadence: ['ci', 'daily', 'weekly'],
+    expected: {
+      framework: '@storybook/vue3-vite',
+      renderer: '@storybook/vue3',
+      builder: '@storybook/builder-vite',
+    },
+  },
+};
+
 export default {
   ...craTemplates,
+  ...nextjsTemplates,
   ...reactViteTemplates,
-  ...vue3ViteTemplates,
   ...svelteViteTemplates,
+  ...vue3ViteTemplates,
   // FIXME: missing documentation.json
   // 'angular/latest': {
   //   name: 'Angular (latest)',
